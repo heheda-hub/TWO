5c8a406302da23f2709726f211ec869ea954ca7a
3067218554 <3067218554@qq.com>
2021-05-20 11:30:54 +0800

    项目创建
    控制台

---
+++ Text/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("nihao");
+        }
+    }
+}
